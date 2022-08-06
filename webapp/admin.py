ab472de3b87b4aa6e223b0c198bdcdbe65748585
shakir <shakirmuhammetov@gmail.com>
2022-08-06 19:13:15 +0600

    Создал модели добавил в Админку

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question']
+    list_display_links = ['question']
+    list_filter = ['question', ]
+    search_fields = ['question', ]
+    fields = ['question']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'poll']
+    list_display_links = ['text']
+    list_filter = ['text', 'poll']
+    search_fields = ['text', ]
+    fields = ['text', 'poll']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=100, null=False, blank=False, verbose_name="Вопрос")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.id}. {self.question}: {self.created_at} "
+
+    class Meta:
+        db_table = "Polls"
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=100, null=False, blank=False, verbose_name="Текст")
+    poll = models.ForeignKey("webapp.Poll", on_delete=models.CASCADE, related_name="poll",
+                             verbose_name="Опрос")
+
+    def __str__(self):
+        return f"{self.text} - {self.poll}"
+
+    class Meta:
+        db_table = "Choice"
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
